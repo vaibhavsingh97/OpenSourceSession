9d55f540d0e5e3450ed76c09450f10ee182d5a85
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2017-09-29 19:46:54 +0530

    initial push

---
+++ staticfiles/static/main/js/scrolling-nav.87ec47ef888d.js
@@ -0,0 +1,20 @@
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1200, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+$(function() {
+    $('a.register-page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1200, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
