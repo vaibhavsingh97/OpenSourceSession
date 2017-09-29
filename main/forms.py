8286aaa7ff52d7848fa4d18286aff60fe5498f2f
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2017-09-29 23:16:04 +0530

    Added form for testing

---
+++ main/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+# our new form
+class RegisterForm(forms.Form):
+    contact_name = forms.CharField(required=True)
+    contact_email = forms.EmailField(required=True)
+    content = forms.CharField(
+        required=True,
+        widget=forms.Textarea
+    )
--- main/views.py
+++ main/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from main.forms import RegisterForm
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def register(request):
+    form_class = RegisterForm
+    
+    return render(request, 'register.html', {
+        'form': form_class,
+    })
