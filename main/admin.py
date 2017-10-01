871c29236fdb0441ce908fe4f8bdebd87ba26687
Bryan Horna <bryanjhv@gmail.com>
2017-10-01 18:15:10 -0500

    Fix link view in panel

--- main/admin.py
+++ main/admin.py
@@ -5,12 +5,14 @@ from main.models import StudentRegisterationForm
 def show_email(obj):
     email = obj.email
     return '<a href="mailto:%s" target="_blank">%s</a>' % (email, email)
+show_email.allow_tags = True
 show_email.short_description = 'Email'
 
 
 def show_github_url(obj):
     github_url = obj.github_url
     return '<a href="%s" target="_blank">%s</a>' % (github_url, github_url)
+show_github_url.allow_tags = True
 show_github_url.short_description = 'GitHub URL'
 
 
