8a059375905b8b5482fd2aa0cea55c7711e3c8f3
Bryan Horna <bryanjhv@gmail.com>
2017-10-01 17:48:51 -0500

    Linkify email and GitHub URL

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from main.models import StudentRegisterationForm
 
+
+def show_email(obj):
+    email = obj.email
+    return '<a href="mailto:%s" target="_blank">%s</a>' % (email, email)
+show_email.short_description = 'Email'
+
+
+def show_github_url(obj):
+    github_url = obj.github_url
+    return '<a href="%s" target="_blank">%s</a>' % (github_url, github_url)
+show_github_url.short_description = 'GitHub URL'
+
+
 class registerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'branch', 'year', 'github_url')
+    list_display = ('name', show_email, 'branch', 'year', show_github_url)
+
     class Meta:
         model = StudentRegisterationForm
 
